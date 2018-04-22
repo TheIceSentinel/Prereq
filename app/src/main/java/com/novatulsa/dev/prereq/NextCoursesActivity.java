dc0abd0c50d55d7347fe08a5c3d3b4e405d580cd
Corey <corjo@okstate.edu>
2018-04-22 16:35:35 -0500

    Button "add" drawable NextActivity to CourseList Button intent

--- app/src/main/java/com/novatulsa/dev/prereq/NextCoursesActivity.java
+++ app/src/main/java/com/novatulsa/dev/prereq/NextCoursesActivity.java
@@ -1,7 +1,10 @@
 package com.novatulsa.dev.prereq;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class NextCoursesActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class NextCoursesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_next_courses);
+
+        // OnClickListener that starts the CourseListActivity to return to adding classes
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(NextCoursesActivity.this, CourseListActivity.class));
+            }
+        });
     }
 }
